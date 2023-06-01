25f467e30147c00a07c962c319430cd3da002b1a
ohbehive <behailukt@gmail.com>
2023-06-01 00:11:39 -0400

    fixes and udpates to node version

--- src/resources/billableMetric.ts
+++ src/resources/billableMetric.ts
@@ -4,4 +4,15 @@ export class BillableMetric extends ApiResource {
     constructor(appId?: string, secretKey?: string, baseUrl?: string) {
         super("billable-metric", appId, secretKey, baseUrl);
     }
+
+    async logUsage(customUid: string, billableMetricId: string, usedAmount: number, version: number = 4) {
+        const path = `/sdk/v${version}/log-billable-metric-usage`;
+        const data = {
+          "custom_uid": customUid,
+          "billable_metric_id": billableMetricId,
+          "used_amount": usedAmount
+        }
+        return this._requestor.request('POST', path, data);
+      }
+    
 }
--- src/resources/token.ts
+++ src/resources/token.ts
@@ -1,5 +1,19 @@
 import { ApiResource } from "./resource";
 
 export class Token extends ApiResource {
-
+    constructor(appId?: string, secretKey?: string, baseUrl?: string) {
+        super("token", appId, secretKey, baseUrl);
+      }
+    
+      async create(params: any, version?: number) {
+        return super.create(version, params);
+      }
+    
+      async getCustomerPortalsToken(customUid: string) {
+        const path = `/customer-portal/tokens/v1/create`;
+        const data = {
+          custom_uid: customUid,
+        };
+        return this._requestor.request("POST", path, data);
+      }
 }
