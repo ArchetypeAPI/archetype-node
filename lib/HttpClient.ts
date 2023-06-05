d3becdb6cf01e1ac10ec052eab484b4c74f51253
Tyler Puleo <tyler@Tylers-MacBook-Pro.local>
2023-06-05 16:55:14 -0700

    changed axios import

--- lib/HttpClient.ts
+++ lib/HttpClient.ts
@@ -1,5 +1,6 @@
 // create an HttpClient class using axios
-const axios = require('axios');
+// const axios = require('axios');
+import axios from 'axios';
 
 class HttpClient {
     private _client: any;
